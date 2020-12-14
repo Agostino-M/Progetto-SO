f81493e69c721a7f104dbf14b525b185d5c0ed92
AgostinoMess <agomessina@gmail.com>
2020-12-14 11:32:01 +0100

    Creazione struct

--- utility.h
+++ utility.h
@@ -1,5 +1,14 @@
 #ifndef UTILITY_H
 #define UTILITY_H
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <signal.h>
 
 #define TEST_ERROR                                 \
     if (errno)                                     \
@@ -12,5 +21,26 @@
                 errno,                             \
                 strerror(errno));                  \
     }
+
+#define REQUEST_LENGTH (sizeof(void) + sizeof(void))
+struct msg_request
+{
+    long mtype;
+    /* data */
+    coordinate start;
+    coordinate end;
+};
+
+typedef struct
+{
+    int row;
+    int column;
+} coordinate;
+
+struct cell
+{
+    int crossing_time;
+    int nmax_taxi;
+};
 
 #endif /* UTILITY_H */

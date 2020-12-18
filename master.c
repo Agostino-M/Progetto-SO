52953f4da4800c58da49d71de8bebd2f75533e04
AgostinoMess <agomessina@gmail.com>
2020-12-18 01:33:47 +0100

    Fix, creazione Taxi

--- master.c
+++ master.c
@@ -2,11 +2,11 @@
 #include "utility.h"
 #include "handling.h"
 
-/* prototipi di funzioni */
-int creaMatrice();
+/* Prototipi di funzioni */
+int create_matrix();
 void fill_resource();
 
-/* variabili globali */
+/* Variabili globali */
 unsigned int SO_HOLES = 10;
 unsigned int SO_SOURCES = 20;
 unsigned int SO_CAP_MIN = 1;
@@ -25,33 +25,40 @@ struct shared_map *city;
 
 int main(int argc, char const *argv[])
 {
-    /* dichiarazione variabili */
-    int id_shd_mem, id_msg_queue, cond, tentativi = 0, i, random_x_p, random_y_p, random_x_a, random_y_a;
+    /* Dichiarazione variabili */
+    int id_shd_mem, id_msg_queue, cond, i, random_x_p, random_y_p, random_x_a, random_y_a;
     struct msg_request request;
-    /* creazione signal header
     struct sigaction sa;
-    bzero(&sa, sizeof(struct sigaction));
-    sa.sa_handler = signal_handler;
-    sigaction(SIGALRM, &sa, NULL);
-    */
+    struct sembuf sem_taxi_w0;
 
-    /* creazione memoria condivisa */
+    /* Creazione signal header */
+    bzero(&sa, sizeof(struct sigaction));
+    sa.sa_handler = &signal_handler;
+    sigaction(SIGALRM, &sa, NULL);
+
+    /* Creazione memoria condivisa */
     id_shd_mem = shmget(IPC_PRIVATE, SHARED_MAP_LENGTH, IPC_CREAT | IPC_EXCL | 0666);
     TEST_ERROR;
 
-    /* attacching memoria condivisa */
+    /* Attacching memoria condivisa */
     city = shmat(id_shd_mem, NULL, 0);
     TEST_ERROR;
 
-    /* creazione array semafori capienza */
+    /* Creazione array semafori capienza */
     id_sem_cap = semget(IPC_PRIVATE, NUM_RISORSE, IPC_CREAT | IPC_EXCL | 0600);
     TEST_ERROR
 
-    /* creazione coda di messaggi */
+    /* creazione semaforo "wait for zero" taxi */
+    id_sem_taxi = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0600);
+    TEST_ERROR
+    set_sem(id_sem_taxi, 0, 1);
+    TEST_ERROR
+
+    /* Creazione coda di messaggi */
     id_msg_queue = msgget(IPC_PRIVATE, IPC_CREAT | IPC_EXCL | 0600);
     TEST_ERROR
 
-    /* creazione matrice */
+    /* Creazione matrice */
     if (SO_HOLES > (ceil(SO_HEIGHT / 2.0) * ceil(SO_WIDTH / 2.0)))
     {
         fprintf(stderr, "Master : impossibile creare la matrice con questi parametri\n");
@@ -61,15 +68,17 @@ int main(int argc, char const *argv[])
     {
         do
         {
-            cond = creaMatrice();
+            cond = create_matrix();
         } while (cond != 0);
     }
 
-    stampa_matrice(city, 3);
+    print_matrix(city, 3);
+
+    /* Inizializzazione del vettore di semafori */
     fill_resource();
     print_resource(id_sem_cap);
 
-    /* creazione richieste */
+    /* Creazione richieste */
     for (i = 0; i < SO_SOURCES; i++)
     {
         switch (fork())
@@ -97,13 +106,18 @@ int main(int argc, char const *argv[])
                 random_y_a = rand() % SO_WIDTH;
             } while (city->matrix[random_x_a][random_y_a].is_hole && random_x_a == random_x_p && random_y_a == random_y_p);
 
+            /* Write del messaggio sulla coda */
             request.mtype = getpid();
             request.start.x = random_x_p;
             request.start.y = random_y_p;
             request.end.x = random_x_a;
             request.end.y = random_y_a;
-            printf("(%d) Nuova richiesta creata : \n - Partenza x : %d \n - Partenza y : %d \n - Arrivo x : %d \n - Arrivo y : %d\n",
-                   getpid(),request.start.x, request.start.y, request.end.x, request.end.y);
+            printf("Source PID:%d: Nuova richiesta creata: \n"
+                   "- Partenza x : %d\n"
+                   "- Partenza y : %d \n"
+                   "- Arrivo x : %d \n"
+                   "- Arrivo y : %d\n",
+                   getpid(), request.start.x, request.start.y, request.end.x, request.end.y);
 
             msgsnd(id_msg_queue, &request, REQUEST_LENGTH, 0);
             TEST_ERROR
@@ -115,37 +129,69 @@ int main(int argc, char const *argv[])
         }
     }
 
-    /* creazione taxi */
+    /* Creazione taxi */
+    for (i = 0; i < SO_TAXI; i++)
+    {
+        switch (fork())
+        {
+        case -1:
+            TEST_ERROR
+            break;
 
-    /* semaforo wait for zero */
+        case 0:
+            /* 
+            * Argomenti args
+            * - argv[1] = id memoria condivisa
+            * - argv[2] = id coda di messaggi per le richieste
+            * - argv[3] = id semaforo delle capacità massime per taxi su una cella
+            * - argv[4] = id semaforo "wait for zero" che dà il via ai taxi
+            */
+            char *args[] = {
+                FILEPATH,
+                id_shd_mem,
+                id_msg_queue,
+                id_sem_cap,
+                id_sem_taxi,
+                NULL};
 
-    /* parte il timer SO_DURATION */
+            execve(FILEPATH, args, NULL);
+            exit(EXIT_FAILURE);
+
+        default:
+            break;
+        }
+    }
+
+    /* Semaforo wait for zero */
+    set_sem(id_sem_taxi, 0, 0);
+    TEST_ERROR
+
+    /* Parte il timer SO_DURATION */
     alarm(SO_DURATION);
 
-    /* detaching ed eliminazione memoria condivisa */
+    /* attesa figli? */
+
+    /* Detaching ed eliminazione memoria condivisa */
     shmdt(city);
     shmctl(id_shd_mem, IPC_RMID, 0);
     TEST_ERROR
 
     /* Eliminazione semafori */
-    semctl(id_sem_cap, IPC_RMID, 0);
-    semctl(id_sem_taxi, IPC_RMID, 0);
-    if (errno == -1)
-    {
-        errno = 0;
-    }
-
-    /* Chiusura coda di messaggi */
-    msgctl(id_msg_queue, IPC_RMID, 0);
+    semctl(id_sem_cap, 0, IPC_RMID);
+    TEST_ERROR
+    semctl(id_sem_taxi, 0, IPC_RMID);
     TEST_ERROR
 
-    sleep(15);
+    /* Chiusura coda di messaggi */
+    msgctl(id_msg_queue, IPC_RMID, NULL);
+    TEST_ERROR
+
     return 0;
 }
 
-int creaMatrice()
+int create_matrix()
 {
-    int i, j, z, i_holes, j_holes, hole_inseriti = 0, cond = 1, tentativi = 0;
+    int i, j, z, i_holes, j_holes, hole_inseriti = 0, cond = 1, attempts = 0;
     unsigned long int random;
 
     srand(getpid());
@@ -170,25 +216,21 @@ int creaMatrice()
         i_holes = rand() % SO_HEIGHT;
         j_holes = rand() % SO_WIDTH;
 
-        if (i_holes == 0) /* caso particolare */
+        if (i_holes == 0) // Caso particolare - aggiungere info */
             i = i_holes;
         else
             i = i_holes - 1;
 
         for (i; i <= i_holes + 1 && cond == 1; i++)
         {
-            if (j_holes == 0) /* caso particolare */
+            if (j_holes == 0) // Caso particolare - aggiungere info */
                 j = j_holes;
             else
                 j = j_holes - 1;
 
             for (j; j <= j_holes + 1; j++)
-            {
                 if (city->matrix[i][j].is_hole == 1)
-                {
                     cond = 0;
-                }
-            }
         }
 
         i = 0;
@@ -203,24 +245,22 @@ int creaMatrice()
         else
         {
             z--;
-            tentativi++;
-            if (tentativi > 30)
-            {
-                return -1; /* Failed */
-            }
+            attempts++;
+            if (attempts > 30)
+                return -1; /* Fallimento */
 
-            continue;
+            continue; // si può omettere?
         }
     }
-
     return 0;
 }
 
 void fill_resource()
 {
+    // Usare sem_set dichiarato in libreria? */
     struct sembuf sops[NUM_RISORSE];
     int i, j, random;
-    srand(time(NULL));
+    srand(getpid());
 
     for (i = 0; i < NUM_RISORSE; i++)
     {
